978fa23c434d253637969702a658d8b2fe646043
Bilenie Tilahun <bilumekb@gmail.com>
2025-04-27 23:29:01 -0500

    write a starting code in my transaction class and main class.

--- src/main/java/com/pluralsight/AccountingLedgerApp.java
+++ src/main/java/com/pluralsight/AccountingLedgerApp.java
@@ -1,7 +1,35 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+import java.util.Scanner;
+
 public class AccountingLedgerApp {
-     {
+    static Scanner myScanner = new Scanner(System.in);
+    public static void main(String[] args) {
+
+
+
+
+
 
     }
+    public static void homeScreen() {
+        System.out.println("Welcome to Accounting Ledger App!!!");
+        System.out.print("What would you like to do?  Select A,B,or C.\n");
+        System.out.print("D)Add Deposit\n");
+        System.out.print("P)Make Payment(Debit)\n");
+        System.out.print("L)Ledger\n");
+        System.out.print("X)Exit");
+    }
+
+
 }
+
--- src/main/java/com/pluralsight/TransactionClass.java
+++ src/main/java/com/pluralsight/TransactionClass.java
@@ -1,6 +1,57 @@
 package com.pluralsight;
 
+import java.time.LocalDate;
+
 public class TransactionClass {
+//attributes/feature what our application should have.
+    private String date;
+    private String time;
+    private String vendor;
+    private String amount;
+
+//Constructor: for setting the requirement of our app / initializing the values.
+
+    public TransactionClass(String date, String time, String vendor, String amount) {
+        this.date = date;
+        this.time = time;
+        this.vendor = vendor;
+        this.amount = amount;
+    }
+
+//Method we need
 
 
+
+//getter and setter for accessing the attributes
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
 }
